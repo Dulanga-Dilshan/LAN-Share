2dc7a06080c80a5c32411e412b336fa7dc723276
Dulanga_Dilshan <dulangadilshan27@gmail.com>
2026-07-28 16:18:39 +0530

    add nginx config and update main py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,File,UploadFile,HTTPException,status
+from fastapi import FastAPI,File,UploadFile,HTTPException,status,APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import uvicorn
@@ -7,7 +7,8 @@ import shutil
 from pathlib import Path
 
 
-app = FastAPI()
+app = FastAPI(root_path="")
+api_router = APIRouter(prefix="/api")
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,14 +17,12 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-upload_dir = "uploads"
 public_dir = 'public'
-os.makedirs(upload_dir,exist_ok=True)
 os.makedirs(public_dir,exist_ok=True)
 
-@app.post('/upload',name="upload_file")
+@api_router.post('/upload',name="upload_file")
 async def handle_uploads(file: UploadFile = File(...)):
-    file_path = os.path.join(upload_dir, file.filename)
+    file_path = os.path.join(public_dir, file.filename)
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -34,25 +33,8 @@ async def handle_uploads(file: UploadFile = File(...)):
         "saved_path": file_path
     }
 
-@app.get('/uploaded',name="uploaded_files")
-async def get_uploaded():
-    path = Path(upload_dir)
-    files = [f.name for f in path.iterdir() if f.is_file()]
-    if len(files)<1:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="no uploaded files"
-        )
 
-    data = {
-        "count":len(files),
-        'files':[
-            file for file in files
-        ]
-    }
-    return data
-
-@app.get('/public',name="public_files")
+@api_router.get('/public',name="public_files")
 async def get_public():
     path = Path(public_dir)
     files = [f.name for f in path.iterdir() if f.is_file()]
@@ -70,25 +52,25 @@ async def get_public():
     }
     return data
 
-@app.get('/download/{filename}',name="download_files")
+@api_router.get('/download/{filename}',name="download_files")
 async def handle_download(filename:str):
     file_path=os.path.join(public_dir,filename)
     if not os.path.exists(file_path):
-        file_path=os.path.join(upload_dir,filename)
-        if not os.path.exists(file_path):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="file not found"
-            )
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="file not found"
+        )
 
     return FileResponse(
         path=file_path,
         filename=filename,
         media_type="application/octet-stream"
     )
-    
 
 
+
+
+app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(
